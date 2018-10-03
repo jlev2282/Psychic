3ea8faf8aaf6e650efd00c93503fa90231d21de6
John <jlev2282@gmail.com>
2018-10-02 20:57:16 -0400

    added score and gameplay function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,27 @@
 "use strict";
 const gameData = {
-    words: ["dark", "October", "pumpkin", "witch", "candy", "costume", "goblin", "frankenstein"]
+    words: ["dark", "October", "pumpkin", "witch", "candy", "costume", "goblin", "frankenstein"],
+    letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ],
+    score: 0,
+    wins: 0,
+    losses: 0,
 }
 
+//function that starts game and picks random word
 function startGame(){
     let word = pickWord();
-    alert("The random word is "+ word);
+    gamePlay(word);
 }
 
+//function that starts game
 function pickWord(){
-    let chosenWord = gameData.words[Math.floor(Math.random() * (gameData.words.length))+1];
+    let chosenWord = gameData.words[Math.floor(Math.random() * gameData.words.length)];
     return chosenWord;
+}
+
+//function that governs game play
+function gamePlay(word) {
+    let wordLength = word.length;
+    let guesses = Math.floor(wordLength / 2);
+    alert(guesses);
 }
